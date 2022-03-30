9f31540b08f9c1ed44efb2eab58cbfd77a2a3bea
Suraj Kumar <102604647+nerdsuraj@users.noreply.github.com>
2022-03-30 15:12:31 +0530

    Add files via upload

---
+++ practice bash/wordcash.sh
@@ -0,0 +1,21 @@
+read -p "enter your single digit number : " a
+
+case $a in
+
+    1)
+
+      echo " one "
+      ;;
+    2)
+
+      echo "two "
+      ;;
+    3)
+
+      echo "three"
+      ;;
+   *)
+     echo "doesnt match with any codition"
+     ;;
+esac
+
