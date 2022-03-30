4f55ef1fd3916c6974b384bc38f879404e0c391a
Suraj Kumar <102604647+nerdsuraj@users.noreply.github.com>
2022-03-30 15:11:17 +0530

    Add files via upload

---
+++ practice bash/weekcash.sh
@@ -0,0 +1,27 @@
+read -p "enter your number  for week : " n
+
+case $n in
+
+	1 )
+
+	 echo " monday "
+
+	;;
+
+	2 )
+
+	 echo " tuesday "
+
+	;;
+
+	3 )
+
+	 echo " wednesday "
+
+	;;
+
+	* )
+
+	 echo " out of number "
+
+esac
